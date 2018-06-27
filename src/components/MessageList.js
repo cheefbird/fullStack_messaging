a713758a0dce6e2be783231b70cd4a87b9d70e1d
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-27 15:42:51 -0700

    Create basic messageList.

---
+++ src/components/MessageList.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { FlatList, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+
+import { MessageShape } from "../../utils/MessageUtils";
+
+const keyExtractor = item => item.id.toString();
+
+export default class MessageList extends Component {
+  static propTypes = {
+    messages: PropTypes.arrayOf(MessageShape).isRequired,
+    onPressMessage: PropTypes.func
+  };
+
+  static defaultProps = {
+    onPressMessage: () => {}
+  };
+
+  renderMessageItem = ({ item }) => {
+    // TODO
+  };
+
+  render() {
+    const { messages } = this.props;
+
+    return (
+      <FlatList
+        style={styles.container}
+        inverted
+        data={messages}
+        renderItem={this.renderMessageItem}
+        keyExtractor={keyExtractor}
+        keyboardShouldPersistTaps={"handled"}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: "visible" // Prevents clipping on resize
+  }
+});
