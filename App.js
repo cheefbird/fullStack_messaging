2ae89a7f22ecedf2d255f74c4a5b2f1f7ccc9eea
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-27 14:04:25 -0700

    Initial commit with work on App.js.

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+export default class App extends Component {
+  renderMessageList() {
+    return <View style={styles.content} />;
+  }
+
+  renderInputMethodEditor() {
+    return <View style={styles.inputMethodEditor} />;
+  }
+
+  renderToolbar() {
+    return <View style={styles.toolbar} />;
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white"
+  }
+});
