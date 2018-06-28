542a38370d4cbe67a097daf95f77181f3a0ceeae
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-27 18:21:44 -0700

    Implement ImageGrid in App.js.

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
 import Status from "./src/components/Status";
 import MessageList from "./src/components/MessageList";
 import Toolbar from "./src/components/Toolbar";
+import ImageGrid from "./src/components/ImageGrid";
 
 import {
   createImageMessage,
@@ -125,9 +126,11 @@ export default class App extends Component {
     );
   }
 
-  renderInputMethodEditor() {
-    return <View style={styles.inputMethodEditor} />;
-  }
+  renderInputMethodEditor = () => (
+    <View style={styles.inputMethodEditor}>
+      <ImageGrid />
+    </View>
+  );
 
   handlePressToolbarCamera = () => {};
 
