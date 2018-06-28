b0b5a4f31010bb0109ce99079e7a661cd0fe7838
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-27 17:31:33 -0700

    Additional work on Toolbar - style and logic.

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -8,7 +8,22 @@ import {
 } from "react-native";
 import PropTypes from "prop-types";
 
+const ToolbarButton = ({ title, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text style={styles.button}>{title}</Text>
+  </TouchableOpacity>
+);
+
+ToolbarButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired
+};
+
 export default class Toolbar extends Component {
+  state = {
+    text: ""
+  };
+
   static propTypes = {
     isFocused: PropTypes.bool.isRequired,
     onChangeFocus: PropTypes.func,
@@ -24,8 +39,41 @@ export default class Toolbar extends Component {
     onPressLocation: () => {}
   };
 
+  handleChangeText = text => {
+    this.setState({ text });
+  };
+
+  handleSubmitEditing = () => {
+    const { onSubmit } = this.props;
+    const { text } = this.state;
+
+    if (!text) return;
+
+    onSubmit(text);
+    this.setState({ text: "" });
+  };
+
   render() {
-    return <View style={styles.toolbar}>{/*...*/}</View>;
+    const { onPressCamera, onPressLocation } = this.props;
+    const { text } = this.state;
+
+    return (
+      <View style={styles.toolbar}>
+        <ToolbarButton title={"C"} onPress={onPressCamera} />
+        <ToolbarButton title={"L"} onPress={onPressLocation} />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid={"transparent"}
+            placeholder={"Type something!"}
+            blurOnSubmit={false}
+            value={text}
+            onChangeText={this.handleChangeText}
+            onSubmitEditing={this.handleSubmitEditing}
+          />
+        </View>
+      </View>
+    );
   }
 }
 
@@ -37,5 +85,25 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingLeft: 16,
     backgroundColor: "white"
+  },
+  button: {
+    top: -2,
+    marginRight: 12,
+    fontSize: 20,
+    color: "gray"
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0,04)",
+    borderRadius: 16,
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    backgroundColor: "rgba(0,0,0,0.02)"
+  },
+  input: {
+    flex: 1,
+    fontSize: 18
   }
 });
